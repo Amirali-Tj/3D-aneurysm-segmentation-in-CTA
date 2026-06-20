6e322a9563533af1096601dbd2ca53a07a1fbcc4
amirali <amirali.tj45@gmail.com>
2026-06-20 19:09:05 +0330

    merge function added

--- aneurysm_3Dsegmentation_in_CTA/inputPipeline/train_test_split.py
+++ aneurysm_3Dsegmentation_in_CTA/inputPipeline/train_test_split.py
@@ -1,7 +1,7 @@
 import os
 from pprint import pprint
 import numpy as np
-
+import nibabel as nib
 
 def dataSplitPerSize(dataPath) :
     source = os.listdir(dataPath)
@@ -62,7 +62,6 @@ def dataSplitPerSample(sets , testRatio , seed=None) :
     return fullTrainSet , fullTestSet
 
 
-
 def dataTensorLoading(dataset) :
     images = []
     labels = []
@@ -84,9 +83,70 @@ def dataTensorLoading(dataset) :
     return images , labels
 
 
-        
-def merge(*label) :
-    label_tensor  = np.array(label)
-    merged_tensor = np.sum(label_tensor , sum=0)
+def dataTensorLoadingNew(dataset) :
+    images = []
+    labels = []
+    merged = []
 
-    return merged_tensor
+    for p in dataset :
+        files = os.listdir(p)
+        label  = []
+        image  = []
+        merged = []
+
+        for file in files :
+            if "label" not in file :
+                image.append(os.path.join(p , file))
+            elif "merge" in file :
+                merged.append(os.path.join(p , file))
+            else :
+                labels.append(os.path.join(p , file))
+        else :
+            if len(merged) == 0 :
+                merged.append("NoMerge")
+        
+        image  = image*len(label)
+        merged = merged*len(label)
+
+        images.extend(image)
+        labels.extend(label)
+        merged.extend(merged)
+
+    return images , labels , merged
+
+
+        
+def merge(filePath) :
+    pFiles = os.listdir(filePath)
+    if len(pFiles) > 2 :
+        image  = None
+        labels = []
+        for name in pFiles :
+            if "label" in name :
+                label = nib.as_closest_canonical(nib.load(os.path.join(filePath , name)))
+                label_array = label.get_fdata()
+                labels.append(label_array)
+            elif "label" not in name :
+                image = name
+        label_tensor  = np.array(labels)
+        merged_tensor = np.sum(label_tensor , axis=0)
+        print(label_tensor.shape)
+
+        imgObj = nib.Nifti1Image(
+            merged_tensor ,
+            affine = label.affine ,
+            header = label.header ,
+        )
+
+        nib.save(
+            imgObj ,
+            os.path.join(filePath ,  image.replace(".nii" , "_merged") + ".nii")
+        )
+        return 1
+    else :
+        return 0
+
+
+#mainPath = "data/temp"
+#for file in os.listdir(mainPath) :
+ #   merge(os.path.join(mainPath , file))
