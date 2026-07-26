425e2e8bd7e55966eeb74f8feab7b8a1cb5c4cd2
amirali <amirali.tj45@gmail.com>
2026-07-26 19:27:03 +0330

    full configuration added

--- aneurysm_3Dsegmentation_in_CTA/model_utility/configuration.py
+++ aneurysm_3Dsegmentation_in_CTA/model_utility/configuration.py
@@ -23,23 +23,52 @@ def unfreeze_model(model , unfreeze_point_name , except_layers_type) :
 
 
 # skip connections
-encoderF_d4 = [ # defualt mode
+encoderF_d4_res18 = [ # defualt mode
     "stage4_unit1_relu1" ,
     "stage3_unit1_relu1" ,
     "stage2_unit1_relu1" ,
     "relu0" ,
 ]
-encoderF_d3 = [
+encoderF_d3_res18 = [
     "stage4_unit1_relu1" ,
     "stage3_unit1_relu1" ,
     "stage2_unit1_relu1" ,
 ]
-encoderF_d2 =[
+encoderF_d3_res18 =[
     "stage4_unit1_relu1" ,
     "stage3_unit1_relu1" ,
 ]
 
+encoderF_d4_res50 = [
+    "activation_65" ,
+    "activation_35" , 
+    "activation_15" ,
+    "activation" ,
+]
+encoderF_d3_res50 = [
+    "activation_65" ,
+    "activation_35" , 
+    "activation_15" ,
+]
+encoderF_d2_res50 = [
+    "activation_65" ,
+    "activation_35" , 
+]
 
-# unfreeze layers 
-unfreeze34_border  = "add_3"
-unfreeze4_border   = "add_5" 
+
+# Res18 Conv_3 and Conv_4 Group 
+
+unfreeze_Conv3_unit1ToEnd_res18   = "stage3_unit1_conv1"
+unfreeze_Conv3_unit2ToEnd_res18   = "stage3_unit2_conv1"
+unfreeze_Conv4_unit1ToEnd_res18   = "stage4_unit1_conv1"
+unfreeze_Conv4_unit2ToEnd_res18   = "stage4_unit2_conv1" 
+
+# Res50 Conv_5 and Conv_4 Group
+
+unfreeze_Conv4_unit5ToEnd_res50   = "conv3d_59"
+unfreeze_Conv4_unit6ToEnd_res50   = "conv3d_64"
+unfreeze_Conv5_unit1ToEnd_res50   = "conv3d_69"
+unfreeze_Conv5_unit2ToEnd_res50   = "conv3d_75"
+unfreeze_Conv5_unit3ToEnd_res50   = "conv3d_80"
+
+
