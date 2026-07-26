9175d2b97d7129199934cec5d531249d95e58848
amirali <amirali.tj45@gmail.com>
2026-07-26 19:26:31 +0330

    augmentation probability control

--- aneurysm_3Dsegmentation_in_CTA/inputPipeline/utility.py
+++ aneurysm_3Dsegmentation_in_CTA/inputPipeline/utility.py
@@ -3,7 +3,12 @@ import numpy as np
 from scipy import ndimage
 import tensorflow as tf
 
-# debug shape problem
+
+def factorial(k) :
+    if k - 1 == 0 :
+        return 1
+    return k * factorial(k - 1)
+
 def read_img(imgPath , labelPath , mergePath) : # fixed
     imgPath   = imgPath.numpy().decode("utf_8")
     labelPath = labelPath.numpy().decode("utf_8")
@@ -183,8 +188,11 @@ class volume_crop : # make it graph compatable
         
 #-----------------------------
 class randomGeo : # should be wrapped with py func
-    def __init__(self , p):
-        self.p = p
+    def __init__(self , p , p_stage="per_stage"):
+        if p_stage == "per_stage" :
+            self.p = p
+        elif isinstance(p_stage , int) and p_stage > 0 :
+            pass
     def rot(self , img_arr , label_arr , * , imgOrder , lblOrder , imgCval , lblCval) :
         angle = tf.random.uniform(shape=() , minval=0 , maxval=360 , dtype=tf.int32)
         chance = tf.random.uniform(shape=()  , dtype=tf.float32)
