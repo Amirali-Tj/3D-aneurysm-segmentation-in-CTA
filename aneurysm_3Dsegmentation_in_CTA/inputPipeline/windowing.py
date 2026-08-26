970100aabc1e3d750b9e12592472013c09375c00
amirali <amirali.tj45@gmail.com>
2026-08-26 18:04:44 +0330

    optimum windowFinder

--- aneurysm_3Dsegmentation_in_CTA/inputPipeline/windowing.py
+++ aneurysm_3Dsegmentation_in_CTA/inputPipeline/windowing.py
@@ -5,7 +5,7 @@ import numpy as np
 
 
 class multiWindowStacking : # cache
-    def __init__(self  , *ranges):
+    def __init__(self  , ranges):
         self.ranges = tf.convert_to_tensor(list(ranges) , dtype=tf.float64)
     def WindowStacking(self , img_arr , label_arr) : # graph compatibale
         def cond(rng , i , img_arr , stackedWindows) :
@@ -40,7 +40,14 @@ class multiWindowStacking : # cache
         label_arr = tf.tile(tf.expand_dims(label_arr , axis=0) , [tf.shape(self.ranges)[0] , 1 , 1 , 1])
         #---
         return img_arr , label_arr
-    
+
+
+class quartileWindowStacking(multiWindowStacking) :
+    def __init__(self) :
+        pass
+    def _quaritleWindowFinder(image_arr , label_arr) : 
+        pass
+
 class randomMultiWindowStackig() : # on-fly and cache
     def __init__(self , default , wwRange , wlRange , p_ww , p_wl): # default as tf.float32
         self.default = tf.expand_dims(tf.convert_to_tensor(default , dtype=tf.float64) , axis=0)
@@ -122,6 +129,8 @@ class randomMultiWindowStackig() : # on-fly and cache
 
 def optimumWindowFinder(dataPath) :
     source = os.listdir(dataPath)
+    allLevel  = []
+    allWidth  = []
 
     for p in source :
         files = os.listdir(os.path.join(dataPath , p))
@@ -138,13 +147,30 @@ def optimumWindowFinder(dataPath) :
 
         # calculating window
         img = nib.as_closest_canonical(nib.load(image))
-        img_arr = img.get_fdata()
+        img_arr = img.get_fdata().astype(np.float32)
 
         for label in labels :
             lbl     = nib.as_closest_canonical(nib.load(label))
-            lbl_arr = label.get_fdata()
+            lbl_arr = lbl.get_fdata().astype(np.float32)
 
-            voi = np.extract(lbl_arr == 1 , img_arr)
-
-            # continued ....
+            # add hu of voi
+            voi   = np.extract(lbl_arr == 1 , img_arr)
+            # calculate upper bound and lower bound
+            Q1   = np.percentile(voi , q=25)
+            Q3   = np.percentile(voi , q=75)
+            IQRT = Q3 - Q1 
+            upperBound = Q3 + 1.5*IQRT
+            lowerBound = Q1 - 1.5*IQRT
+            meanLevel  = (upperBound + lowerBound)/2 
+            allLevel.append(meanLevel)
+            allWidth.append(upperBound - lowerBound)
+        else :
+            print(f"patinet {p} analyzed")
+    
+    return {
+        "levelMin" : np.min(allLevel) , 
+        "levelMax" : np.max(allLevel) ,
+        "widthMin" : np.min(allWidth) , 
+        "widthMax" : np.min(allWidth)  
+    }
 
