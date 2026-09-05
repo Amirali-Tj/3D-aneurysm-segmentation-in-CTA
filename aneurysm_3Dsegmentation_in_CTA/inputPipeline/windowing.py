a19039ed7f439a0ac50196fc2dea149461ae4b50
amirali <amirali.tj45@gmail.com>
2026-09-05 12:45:50 +0330

    optimization on windowing operations

--- aneurysm_3Dsegmentation_in_CTA/inputPipeline/windowing.py
+++ aneurysm_3Dsegmentation_in_CTA/inputPipeline/windowing.py
@@ -64,84 +64,64 @@ class quartileWindowStacking(multiWindowStacking) :
         self._quaritleWindowFinder(image_arr , label_arr)
         image_arr , label_arr = super().WindowStacking(image_arr , label_arr)
         return image_arr , label_arr
+
+class windowing :
+    def __init__(self , ww , wl):
+        self.wl = wl
+        self.ww = ww
+    def apply(self , img , label) :
+        minHu = self.wl - (self.ww/2)
+        maxHu = self.wl + (self.ww/2)
         
-class randomMultiWindowStackig() : # on-fly and cache
+        img = tf.clip_by_value(img , minHu , maxHu)
+        return img , label
+
+
+class randomWindowing(windowing) : # on-fly
     def __init__(self , default , wwRange , wlRange , p_ww , p_wl): # default as tf.float32
-        self.default = tf.expand_dims(tf.convert_to_tensor(default , dtype=tf.float64) , axis=0)
-        self.wwRange = tf.expand_dims(tf.convert_to_tensor(wwRange , dtype=tf.float64) , axis=0)
-        self.wlRange = tf.expand_dims(tf.convert_to_tensor(wlRange , dtype=tf.float64) , axis=0)
-        self.p_ww    = tf.convert_to_tensor(p_ww , dtype=tf.float64)
-        self.p_wl    = tf.convert_to_tensor(p_wl , dtype=tf.float64)
+        self.default = tf.convert_to_tensor(default , dtype=tf.float32)
+        super().__init__(self.default[0] , self.default[1])
+
+        self.wwRange = tf.convert_to_tensor(wwRange , dtype=tf.float32)
+        self.wlRange = tf.convert_to_tensor(wlRange , dtype=tf.float32)
+        self.p_ww    = tf.convert_to_tensor(p_ww , dtype=tf.float32)
+        self.p_wl    = tf.convert_to_tensor(p_wl , dtype=tf.float32)
     def WindowStacking(self , img_arr , label_arr) : # graph compatibale
         rGenWL = tf.cond(
             tf.random.uniform(shape=() , dtype=tf.float64) <= self.p_wl ,
             lambda : tf.random.uniform(
                 shape=() ,
-                minval=self.wlRange[0][0] ,
-                maxval=self.wlRange[0][1] ,
-                dtype=tf.float64
+                minval=self.wlRange[0] ,
+                maxval=self.wlRange[1] ,
+                dtype=tf.float32
             ),
-            lambda : self.default[0][0]
+            lambda : self.default[0]
             )
         rGenWW = tf.cond(
             tf.random.uniform(shape=() , dtype=tf.float64) <= self.p_ww ,
             lambda : tf.random.uniform(
                 shape=() ,
-                minval=self.wwRange[0][0] ,
-                maxval=self.wwRange[0][1] ,
-                dtype=tf.float64
+                minval=self.wwRange[0] ,
+                maxval=self.wwRange[1] ,
+                dtype=tf.float32
             ) ,
-            lambda : self.default[0][1]
+            lambda : self.default[1]
             )
-        self.ranges = tf.convert_to_tensor([(rGenWL , rGenWW)])
         #-----
-        def cond(rng , i , img_arr , stackedWindows) :
-            return i < tf.shape(rng)[0]
-        def body(rng , i , img_arr , stackedWindows) :
-            wl = rng[i][0]
-            ww = rng[i][1]
-            minHU = wl - (ww/2)
-            maxHU = wl + (ww/2)
-            windowChannel  = tf.expand_dims(tf.clip_by_value(img_arr , minHU , maxHU) , axis=0)
-            stackedWindows = tf.concat([stackedWindows , windowChannel] , axis=0)
-            i+=1
-            return rng , i , img_arr , stackedWindows
-        _ , _ , _ , windows = tf.while_loop(
-            cond ,
-            body ,
-            loop_vars=[
-                self.ranges ,
-                0 , 
-                img_arr ,
-                tf.expand_dims(tf.zeros(tf.shape(img_arr) , dtype=tf.float64) , axis=0)
-            ] , 
-            shape_invariants = [
-                self.ranges.get_shape(),
-                tf.TensorShape(()) ,
-                img_arr.get_shape() , 
-                tf.TensorShape([None , None , None , None])
-            ]
-        )
-        #---
-        img_arr   = windows[1: , : , : , :]
-        label_arr = tf.tile(tf.expand_dims(label_arr , axis=0) , [tf.shape(self.ranges)[0] , 1 , 1 , 1]) # redundant line
-        #---
+        minHu = rGenWL - (rGenWW/2)
+        maxHu = rGenWL + (rGenWW/2)
+
+        img_arr   = tf.expand_dims(img_arr , axis=0)   # add channel dim
+        label_arr = tf.expand_dims(label_arr , axis=0) # add channel dim
+
+        img_arr = tf.clip_by_value(img_arr , minHu , maxHu)
+
         return img_arr , label_arr
     def apply_default(self , img , label) :
-        defaultWL = self.default[0][0]
-        defaultWW = self.default[0][1]
 
-        minHU = defaultWL - (defaultWW/2)
-        maxHU = defaultWL + (defaultWW/2)
-
-        img = tf.clip_by_value(
-            img ,
-            minHU ,
-            maxHU
-        )
+        img , label = self.apply(img , label)
 
         return img , label
-
 
 
 def optimumWindowFinder(dataPath) :
